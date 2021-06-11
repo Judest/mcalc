ec9ac7ddd9680d1744e6705d27fd166d1111cc92
Judest <85638276+Judest@users.noreply.github.com>
2021-06-11 21:05:21 +0300

    Add files via upload

---
+++ Material.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    class Material
+    {
+        public static Material[] ReadFromCsvFile(string fileName)
+        {
+            return
+            File
+            .ReadLines(fileName)
+            .Select(x =>
+            {
+                var array = x.Split(',');
+                return new Material
+                {
+                    Name = array[0],
+                    Density = double.Parse(array[1])
+                };
+            }).ToArray();
+        }
+
+        public string Name;
+        public double Density;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static Material[] materials = Material.ReadFromCsvFile("Materials.csv");
+        //new[]
+        //{
+        //    new Material{ Name = "Алюминий", Density = 2.7 },
+        //    new Material{ Name = "Медь", Density = 4.5 },
+        //};
+        static void countWeight(double densityMat)
+        {
+            double widthAluminum = readFromConsole("Какова ширина шины в мм");
+            double thicknessAluminum = readFromConsole("Какова толщина шины в мм");
+            double lengthAluminum = readFromConsole("Какова длина шины в м") / 1000.0;
+            double resForAlu = (densityMat * widthAluminum * thicknessAluminum * lengthAluminum);
+
+            Console.WriteLine($"{Math.Round(resForAlu, 3)} кг");
+        }
+        static int readFromConsole(string qwerty)
+        {
+            double numberOne;
+            string strOne;
+            int count = 3;
+
+            do
+            {
+
+                if (count <= 0)
+                {
+                    Console.WriteLine("С вас достаточно");
+                    throw new Exception();
+
+                }
+                if (count-- == 1)
+                {
+                    Console.WriteLine("Последнее предупреждение");
+                }
+
+                Console.WriteLine(qwerty);
+                strOne = Console.ReadLine();
+                
+                if (double.TryParse(strOne,  out numberOne))
+                {
+                    break;
+                }
+                Console.WriteLine("Мама, слепи мне снежок!");
+
+
+            }
+            while (true);
+
+            return (int)numberOne;
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo key;
+
+            Console.WriteLine("Добро пожаловать в калькулятор шин");
+            try
+            {
+                do
+                {
+                    Console.WriteLine("\n\nДоступные материалы:");
+                    for (int i = 0; i < materials.Length; i++)
+                    {
+                        Console.WriteLine($"{i}: {materials[i].Name}");
+                    }
+
+                    int choiceMaterial = readFromConsole("Выберите материал");
+                    if (choiceMaterial >= 0 && choiceMaterial < materials.Length)
+                    {
+                        Console.WriteLine($"Считаем {materials[choiceMaterial].Name}");
+                        countWeight(materials[choiceMaterial].Density);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Сразу видно вы мудак");
+                    }
+
+                    Console.WriteLine("Для продолжения нажмите любую клавишу");
+                    Console.WriteLine("Для выхода нажмите Escape");
+                    key = Console.ReadKey();
+                }
+
+                while (key.Key != ConsoleKey.Escape);
+            }
+            catch { }
+       
+        }
+    }
+}
